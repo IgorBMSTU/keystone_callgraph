a3d6cad95ca0d00ff6026185eac705d22305093f
IgorBMSTU <igor95n@gmail.com>
2016-04-23 18:46:49 -0400

    modified:   generate_data.py 	modified:   keystone_callgraph.py

--- generate_data.py
+++ generate_data.py
@@ -1,81 +1,74 @@
 import os
-import datetime
-import pprint
-import pstats
 import marshal
-import matplotlib
-matplotlib.use('Agg')
-from numpy import array
-from scipy import stats
 import numpy as np
-import pylab
 import pickle
 from six import iteritems
 
+LOW_RPS_FOLDER = "/var/lib/werkzeug_floder/uwsgi_10"
+MEDIUM_RPS_FOLDER = "/var/lib/werkzeug_floder/uwsgi_60"
+HIGH_RPS_FOLDER = "/var/lib/werkzeug_floder/uwsgi_111"
+
 class Pstat:
     def __init__(self, outp_dir):
-        self.func_dict = {}
         self.outp_dir = outp_dir
+        self.func_dict = {}
 
-    def parse_stat(self, stats):
+    def fill_fulldict(self, stats):
         for func, (cc, nc, tt, ct, callers) in iteritems(stats):
             if not func in self.func_dict:
-                self.func_dict[func] = []
-            self.func_dict[func].append([float(ct)/float(cc), ct, cc])
-
+                self.func_dict[func] = {}
+                self.func_dict[func]["cumtime_values"] = []
+                self.func_dict[func]["call_values"] = []
+                self.func_dict[func]["percall_values"] = []
+            self.func_dict[func]["percall_values"].append(float(ct)/float(cc))
+            self.func_dict[func]["cumtime_values"].append(ct)
+            self.func_dict[func]["call_values"].append(cc)
 
     def read_stat(self, fname):
         with open("%s/%s" % (self.outp_dir, fname), "rb") as f:
             data = marshal.load(f)
             return data
 
-    def get_flist(self, directory):
-        unsorted_flist = []
-        f_names = filter(lambda x: x.endswith('.prof'), os.listdir(directory))
-        for fname in f_names:
-            timestamp = float(fname.split(".")[-2])
-            unsorted_flist.append([timestamp, fname])
-        unsorted_flist.sort()
-        return unsorted_flist
 
 
-    def sort_flist(self):
-        sorted_flist = os.listdir(self.outp_dir)
-        if not len(self.func_dict):
-            for file_ in sorted_flist:
-                try:
-                    d1 = self.read_stat(file_)
-                    self.parse_stat(d1)
-                except:
-                    print "#1", file_
+    def load_profiler_data(self):
+        flist = os.listdir(self.outp_dir)
+        if not self.func_dict:
+            for file_ in flist:
+                if os.path.exists("%s/%s" % (self.outp_dir, file_)):
+                    marshal_data = self.read_stat(file_)
+                    self.fill_fulldict(marshal_data)
 
-    def gen_arrays(self):
+    def count_avg(self):
         res_dict = {}
         for fpath in self.func_dict:
-            div_arr = [val[0] for val in self.func_dict[fpath]]
-            ct_arr = [val[1] for val in self.func_dict[fpath]]
-            cc_arr = [val[2] for val in self.func_dict[fpath]]
-            res_dict[fpath] = [np.average(div_arr), np.average(ct_arr), sum(cc_arr)]
+            cumtime_list = self.func_dict[fpath]["cumtime_values"]
+            percall_list = self.func_dict[fpath]["percall_values"]
+            cc_list = self.func_dict[fpath]["call_values"]
+            res_dict[fpath] = { "avg_cumtime": np.average(cumtime_list),
+                                "avg_percall": np.average(percall_list),
+                                "number_of_calls": sum(cc_list)
+                }
         return res_dict
 
 
 
-
 if __name__ == "__main__":
-    first_test= Pstat("/home/modis/werkzeug_data/1.5min_10")
-    first_test.sort_flist()
-    D1 = first_test.gen_arrays()
-    with open('wergzeug_10rps.pickle', 'wb') as handle:
-        pickle.dump(D1, handle)
+    first_test = Pstat(LOW_RPS_FOLDER)
+    first_test.load_profiler_data()
+    low_data = first_test.count_avg()
+    with open('werkzeug_low_rps.pickle', 'wb') as handle:
+        pickle.dump(low_data, handle)
 
-    second_test = Pstat("/home/modis/werkzeug_data/1.5min_60")
-    second_test.sort_flist()
-    D2 = second_test.gen_arrays()
-    with open('wergzeug_60rps.pickle', 'wb') as handle:
-        pickle.dump(D2, handle)
+    second_test = Pstat(MEDIUM_RPS_FOLDER)
+    second_test.load_profiler_data()
+    medium_data = second_test.count_avg()
+    with open('werkzeug_medium_rps.pickle', 'wb') as handle:
+        pickle.dump(medium_data, handle)
 
-    third_test = Pstat("/home/modis/werkzeug_data/1.5min_111")
-    third_test.sort_flist()
-    D3 = third_test.gen_arrays()
-    with open('wergzeug_111rps.pickle', 'wb') as handle:
-        pickle.dump(D3, handle)
+    third_test = Pstat(HIGH_RPS_FOLDER)
+    third_test.load_profiler_data()
+    high_data = third_test.count_avg()
+    with open('werkzeug_high_rps.pickle', 'wb') as handle:
+        pickle.dump(high_data, handle)
+
--- keystone_callgraph.py
+++ keystone_callgraph.py
@@ -1,81 +1,86 @@
+import ast
 import pickle
 import pygraphviz as pgv
+from os.path import exists
 
-ORIGINAL_GRAPH_PATH = '/home/igor/graph1min.dot'
+
+ORIGINAL_GRAPH_PATH = '/home/modis/graph1min.dot'
 ROOT_FUNCTION = "webob.dec.wsgify.__call__"
 GRAPH_FNAME = 'last_outp'
 
-def load_werkzeug_data():
-    degr_path_list = []
-    with open('wergzeug_10rps.pickle', 'rb') as handle:
-        wergzeug_10rps = pickle.load(handle)
+ROOT_COLOR = "#4900F5"
+DEGR_NODES_COLOR = "#FF6500"
+OTHER_NODES_COLOR = "#E4E4E4"
 
-    with open('wergzeug_60rps.pickle', 'rb') as handle:
-        wergzeug_60rps = pickle.load(handle)
+def load_werkzeug_data(min_div = 1, min_sub=0, min_time=0):
+    degr_path_list = []#defaultdict(dict)
+    with open('werkzeug_low_rps.pickle', 'rb') as handle:
+        low_rps_data = pickle.load(handle)
 
-    with open('wergzeug_111rps.pickle', 'rb') as handle:
-        wergzeug_111rps = pickle.load(handle)
+    with open('werkzeug_medium_rps.pickle', 'rb') as handle:
+        medium_rps_data = pickle.load(handle)
 
-    for file_path in wergzeug_10rps:
-        try:
-            avg_time_1 = wergzeug_10rps[file_path][0]
-            avg_time_2 = wergzeug_60rps[file_path][0]
-            avg_time_3 = wergzeug_111rps[file_path][0]
-            avg_calls_3 = wergzeug_111rps[file_path][2]
-            if avg_time_1 < avg_time_2 and avg_time_2 <  avg_time_3:
-                sub_1 = avg_time_3 - avg_time_2
-                sub_2 = avg_time_2 - avg_time_1
-                if sub_2 < sub_1:
-                    degr_path_list.append([avg_calls_3 * avg_time_3, sub_1, sub_2, file_path])
-        except:
-            pass#print("err, #3", file_path)
+    with open('werkzeug_high_rps.pickle', 'rb') as handle:
+        high_rps_data = pickle.load(handle)
+    
+    for file_path in low_rps_data:
+        if file_path in medium_rps_data and file_path in high_rps_data:
+            avg_time_low = low_rps_data[file_path]["avg_percall"]
+            avg_time_medium = medium_rps_data[file_path]["avg_percall"]
+            avg_time_high = high_rps_data[file_path]["avg_percall"]
+            avg_calls_high = high_rps_data[file_path]["number_of_calls"]
+            if avg_time_low < avg_time_medium and avg_time_medium <  avg_time_high:
+                sub_right = avg_time_high - avg_time_medium
+                sub_left = avg_time_medium - avg_time_low
+                if sub_right > sub_left:
+                    if sub_right / sub_left >= min_div and sub_right-sub_left >= min_sub and avg_time_high*avg_calls_high >= min_time:
+                        degr_path_list.append(file_path)
+    print len(degr_path_list)
+                    #degr_path_list[file_path]["avg"] = avg_calls_high * avg_time_high
+
+    #.append([avg_calls_high * avg_time_high, sub_right, sub_left, file_path])
+    
+    #print filter(lambda tt, sr, sl, fp: sr/sl >= min_div and sr-sl >= min_sub and tt >= min_time, degr_path_list)
+    #files = []
+    #for time, sub_right, sub_left, file_path in degr_path_list:
+    #    if sub_right / sub_left >= min_div and sub_right-sub_left >= min_sub and time >= min_time:
+    #        files.append(file_path)
+    #return files
+    
     return degr_path_list
 
 
+    
+def find_classname(func_data):
+    path, strnumb, name = func_data
+    if exists(path):
+        with open(path, "r") as f:
+            source = f.read()
+            tree = ast.parse(source)
+            for obj in tree.body:
+                if isinstance(obj, ast.ClassDef):
+                    for line in obj.body: #i.body
+                        #if isinstance(j, ast.FunctionDef):
+                        if int(line.lineno) == strnumb:
+                            return "%s.%s.%s" % (path.replace(".py", "").split("/")[-1], obj.name, name)
+    return "%s.%s" % (path.replace(".py", "").split("/")[-1], name)
+
+def get_degr_list(files):
+    nodes_for_save = []
+    for path_tuple in files:
+        node_ends_with = find_classname(path_tuple)
+        for node in original_graph.nodes():
+            if node.endswith(node_ends_with):
+                nodes_for_save.append(node)
+    return nodes_for_save    
+
+    
 def limit(min_div, min_sub=0, min_time=0):
     files = []
-    for time, sub_1, sub_2, file_path in degr_path_list:
-        if sub_1 / sub_2 >= min_div and sub_1-sub_2 >= min_sub and time >= min_time:
-            files.append((file_path[0].replace(".py", ""), file_path[2]))
+    for time, sub_right, sub_left, file_path in degr_path_list:
+        if sub_right / sub_left >= min_div and sub_right-sub_left >= min_sub and time >= min_time:
+            files.append(file_path)
     return files
-
-
-def is_restricted(node_): # FIRST
-    path_with_class = node_.split(".")
-    return_value = False
-    for file in files:
-        cl_funcname = node_.split(".")[-1]
-        dir_funcname = file[1]
-        if cl_funcname == dir_funcname:
-            dir_path = file[0].replace("keystone/venv/local/", "")
-            rel_path = dir_path.split("/") + [dir_funcname]
-            unwanted_words = ["",
-                    "home",
-                    "modis",
-                    "lib",
-                    "python2.7",
-                    "site-packages"]
-            rel_path = [word for word in rel_path if not word in unwanted_words]
-            for_del = []
-            for word in path_with_class:
-                if [letter.isupper() for letter in word].count(1) > 0 or word == "common":
-                    for_del.append(word)
-                    if word in rel_path:
-                        rel_path.remove(word)
-            for j in for_del:
-                path_with_class.remove(j)
-            if path_with_class == rel_path:
-                return_value = True
-    return return_value
-
-
-def get_restr_list():
-    restr_list = []
-    for node in original_graph.nodes():
-        if is_restricted(node):
-            restr_list.append(node)
-    return restr_list
-
 
 def set_color(node_name, color):
     node_obj = original_graph.get_node(node_name)
@@ -93,40 +98,42 @@ def get_predecessors(fnode):
 def shortest_paths(prev_flist):
     next_flist = []
     for flist in prev_flist:
-        next_flist += [flist + [parent] for parent in get_predecessors(flist[-1]) if not parent in flist]
-    if [function[-1] for function in next_flist].count(ROOT_FUNCTION) > 0:
-        return [j for j in next_flist if j[-1] == ROOT_FUNCTION]
+        next_flist += [flist + [parent] for parent in get_predecessors(flist[-1])]
+    next_filtered = filter(lambda j: j[-1] == ROOT_FUNCTION, next_flist)
+    if next_filtered:
+        return next_filtered
     return shortest_paths(next_flist)
 
 
 def del_unmarked_nodes():
-    marks = {}
+    marked = []
     for node_key in restr_list:
         init_flist = [[node_key]]
         for path in shortest_paths(init_flist):
-            #print len(path),  path
             for save_node in path:
-                marks[save_node] = 2
-                set_color(save_node, "#E4E4E4")
+                marked.append(save_node)
+                set_color(save_node, OTHER_NODES_COLOR)
             req_func = path[0]
-            marks[req_func] = 1
-            set_color(req_func, "#FF6500")
+            marked.append(req_func)
+            set_color(req_func, DEGR_NODES_COLOR)
     for node in original_graph.nodes():
-        if not node in marks:
+        if not node in marked:
             original_graph.remove_node(node)
 
 
 def save_to_dot():
-    set_color(ROOT_FUNCTION, "#4900F5")
+    set_color(ROOT_FUNCTION, ROOT_COLOR)
     original_graph.write("%s.dot" % GRAPH_FNAME)
 
 if __name__ == "__main__":
-    original_graph=pgv.AGraph(ORIGINAL_GRAPH_PATH)
-    degr_path_list = load_werkzeug_data()
-    files = limit(min_div=50, min_sub = 0, min_time=10)
-    restr_list = get_restr_list()
+    original_graph = pgv.AGraph(ORIGINAL_GRAPH_PATH)
+    files = load_werkzeug_data(min_div = 1, min_sub = 0, min_time = 0)
+    #files = limit(min_div = 5, min_sub = 0, min_time = 2)
+    restr_list = get_degr_list(files)
     del_unmarked_nodes()
+    print 1
     save_to_dot()
     original_graph.layout()
     original_graph.draw("%s.svg" % GRAPH_FNAME, prog='dot')
     print "number of nodes: ", len(original_graph.nodes())
+
