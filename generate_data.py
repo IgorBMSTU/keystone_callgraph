860295b2222eb6b06d5e69a3a66e88535688b8a3
IgorBMSTU <igor95n@gmail.com>
2016-04-24 06:49:30 -0400

    modified:   generate_data.py 	modified:   keystone_callgraph.py

--- generate_data.py
+++ generate_data.py
@@ -5,8 +5,13 @@ import pickle
 from six import iteritems
 
 LOW_RPS_FOLDER = "/var/lib/werkzeug_floder/uwsgi_10"
+LOW_RPS_DATA = "werkzeug_low_rps.pickle"
+
 MEDIUM_RPS_FOLDER = "/var/lib/werkzeug_floder/uwsgi_60"
+MEDIUM_RPS_DATA = 'werkzeug_medium_rps.pickle'
+
 HIGH_RPS_FOLDER = "/var/lib/werkzeug_floder/uwsgi_111"
+HIGH_RPS_DATA = 'werkzeug_high_rps.pickle'
 
 class Pstat:
     def __init__(self, outp_dir):
@@ -40,16 +45,16 @@ class Pstat:
                     self.fill_fulldict(marshal_data)
 
     def count_avg(self):
-        res_dict = {}
+        avg_dict = {}
         for fpath in self.func_dict:
             cumtime_list = self.func_dict[fpath]["cumtime_values"]
             percall_list = self.func_dict[fpath]["percall_values"]
             cc_list = self.func_dict[fpath]["call_values"]
-            res_dict[fpath] = { "avg_cumtime": np.average(cumtime_list),
+            avg_dict[fpath] = { "avg_cumtime": np.average(cumtime_list),
                                 "avg_percall": np.average(percall_list),
                                 "number_of_calls": sum(cc_list)
                 }
-        return res_dict
+        return avg_dict
 
 
 
@@ -57,18 +62,18 @@ if __name__ == "__main__":
     first_test = Pstat(LOW_RPS_FOLDER)
     first_test.load_profiler_data()
     low_data = first_test.count_avg()
-    with open('werkzeug_low_rps.pickle', 'wb') as handle:
+    with open(LOW_RPS_DATA, 'wb') as handle:
         pickle.dump(low_data, handle)
 
     second_test = Pstat(MEDIUM_RPS_FOLDER)
     second_test.load_profiler_data()
     medium_data = second_test.count_avg()
-    with open('werkzeug_medium_rps.pickle', 'wb') as handle:
+    with open(MEDIUM_RPS_DATA, 'wb') as handle:
         pickle.dump(medium_data, handle)
 
     third_test = Pstat(HIGH_RPS_FOLDER)
     third_test.load_profiler_data()
     high_data = third_test.count_avg()
-    with open('werkzeug_high_rps.pickle', 'wb') as handle:
+    with open(HIGH_RPS_DATA, 'wb') as handle:
         pickle.dump(high_data, handle)
 
--- keystone_callgraph.py
+++ keystone_callgraph.py
@@ -3,7 +3,6 @@ import pickle
 import pygraphviz as pgv
 from os.path import exists
 
-
 ORIGINAL_GRAPH_PATH = '/home/modis/graph1min.dot'
 ROOT_FUNCTION = "webob.dec.wsgify.__call__"
 GRAPH_FNAME = 'last_outp'
@@ -12,15 +11,19 @@ ROOT_COLOR = "#4900F5"
 DEGR_NODES_COLOR = "#FF6500"
 OTHER_NODES_COLOR = "#E4E4E4"
 
+LOW_RPS_DATA = "werkzeug_low_rps.pickle"
+MEDIUM_RPS_DATA = 'werkzeug_medium_rps.pickle'
+HIGH_RPS_DATA = 'werkzeug_high_rps.pickle'
+
 def load_werkzeug_data(min_div = 1, min_sub=0, min_time=0):
-    degr_path_list = []#defaultdict(dict)
-    with open('werkzeug_low_rps.pickle', 'rb') as handle:
+    degr_path_list = []
+    with open(LOW_RPS_DATA, 'rb') as handle:
         low_rps_data = pickle.load(handle)
 
-    with open('werkzeug_medium_rps.pickle', 'rb') as handle:
+    with open(MEDIUM_RPS_DATA, 'rb') as handle:
         medium_rps_data = pickle.load(handle)
 
-    with open('werkzeug_high_rps.pickle', 'rb') as handle:
+    with open(HIGH_RPS_DATA, 'rb') as handle:
         high_rps_data = pickle.load(handle)
     
     for file_path in low_rps_data:
@@ -32,24 +35,12 @@ def load_werkzeug_data(min_div = 1, min_sub=0, min_time=0):
             if avg_time_low < avg_time_medium and avg_time_medium <  avg_time_high:
                 sub_right = avg_time_high - avg_time_medium
                 sub_left = avg_time_medium - avg_time_low
-                if sub_right > sub_left:
-                    if sub_right / sub_left >= min_div and sub_right-sub_left >= min_sub and avg_time_high*avg_calls_high >= min_time:
+                div = sub_right / sub_left
+                sub = sub_right - sub_left
+                time = avg_time_high * avg_calls_high
+                if sub_right > sub_left and div >= min_div and sub >= min_sub and time >= min_time:
                         degr_path_list.append(file_path)
-    print len(degr_path_list)
-                    #degr_path_list[file_path]["avg"] = avg_calls_high * avg_time_high
-
-    #.append([avg_calls_high * avg_time_high, sub_right, sub_left, file_path])
-    
-    #print filter(lambda tt, sr, sl, fp: sr/sl >= min_div and sr-sl >= min_sub and tt >= min_time, degr_path_list)
-    #files = []
-    #for time, sub_right, sub_left, file_path in degr_path_list:
-    #    if sub_right / sub_left >= min_div and sub_right-sub_left >= min_sub and time >= min_time:
-    #        files.append(file_path)
-    #return files
-    
     return degr_path_list
-
-
     
 def find_classname(func_data):
     path, strnumb, name = func_data
@@ -59,9 +50,8 @@ def find_classname(func_data):
             tree = ast.parse(source)
             for obj in tree.body:
                 if isinstance(obj, ast.ClassDef):
-                    for line in obj.body: #i.body
-                        #if isinstance(j, ast.FunctionDef):
-                        if int(line.lineno) == strnumb:
+                    for line in obj.body:
+                        if line.lineno == strnumb:
                             return "%s.%s.%s" % (path.replace(".py", "").split("/")[-1], obj.name, name)
     return "%s.%s" % (path.replace(".py", "").split("/")[-1], name)
 
@@ -74,38 +64,26 @@ def get_degr_list(files):
                 nodes_for_save.append(node)
     return nodes_for_save    
 
-    
-def limit(min_div, min_sub=0, min_time=0):
-    files = []
-    for time, sub_right, sub_left, file_path in degr_path_list:
-        if sub_right / sub_left >= min_div and sub_right-sub_left >= min_sub and time >= min_time:
-            files.append(file_path)
-    return files
-
 def set_color(node_name, color):
     node_obj = original_graph.get_node(node_name)
     node_obj.attr['fillcolor']=color
 
-
-
 def get_predecessors(fnode):
     all_edges = set()
     for edges_tuple in original_graph.in_edges(fnode):
-        for edge in edges_tuple:
-            all_edges.add(edge)
+        all_edges.add(edges_tuple[0])
     return list(all_edges)
 
 def shortest_paths(prev_flist):
     next_flist = []
     for flist in prev_flist:
-        next_flist += [flist + [parent] for parent in get_predecessors(flist[-1])]
+        next_flist += [flist + [i] for i in get_predecessors(flist[-1])]
     next_filtered = filter(lambda j: j[-1] == ROOT_FUNCTION, next_flist)
     if next_filtered:
         return next_filtered
     return shortest_paths(next_flist)
 
-
-def del_unmarked_nodes():
+def del_unmarked_nodes(restr_list):
     marked = []
     for node_key in restr_list:
         init_flist = [[node_key]]
@@ -120,20 +98,16 @@ def del_unmarked_nodes():
         if not node in marked:
             original_graph.remove_node(node)
 
-
-def save_to_dot():
+def save_to_dot(original_graph):
     set_color(ROOT_FUNCTION, ROOT_COLOR)
     original_graph.write("%s.dot" % GRAPH_FNAME)
 
 if __name__ == "__main__":
     original_graph = pgv.AGraph(ORIGINAL_GRAPH_PATH)
-    files = load_werkzeug_data(min_div = 1, min_sub = 0, min_time = 0)
-    #files = limit(min_div = 5, min_sub = 0, min_time = 2)
+    files = load_werkzeug_data(min_div = 8, min_sub = 0, min_time = 3)
     restr_list = get_degr_list(files)
-    del_unmarked_nodes()
-    print 1
-    save_to_dot()
+    del_unmarked_nodes(restr_list)
+    save_to_dot(original_graph)
     original_graph.layout()
     original_graph.draw("%s.svg" % GRAPH_FNAME, prog='dot')
     print "number of nodes: ", len(original_graph.nodes())
-
